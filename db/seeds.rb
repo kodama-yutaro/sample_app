1b3e495bd57702e966a74849fc6c51ead3cac695
kodamayutaro <kodamayutaro@gmail.com>
2017-08-25 19:49:08 +0900

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,10 +16,18 @@ User.create!(name:  "Example User",
                password_confirmation: password,
               activated: true,
               activated_at: Time.zone.now)
+end
 
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
-end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
